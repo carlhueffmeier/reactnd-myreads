44ad644966dfab02ca476e4138c97844b95c8b4a
Carl Hüffmeier <carlhueffmeier@gmail.com>
2017-08-30 21:20:16 +0200

    feat: Add routes with react-router.

--- src/ListBooks.js
+++ src/ListBooks.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import Bookshelf from './Bookshelf'
 
 const ListBooks = (props) => {
@@ -23,7 +24,7 @@ const ListBooks = (props) => {
           />
         </div>
         <div className="open-search">
-          <a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>
+          <Link to='/search'>Add a book</Link>
         </div>
       </div>
     </div>
