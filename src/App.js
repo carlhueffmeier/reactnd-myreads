51975f34b8d4da758e5e3de29616ad75ffe100a3
Carl Hüffmeier <carlhueffmeier@gmail.com>
2017-09-01 16:09:53 +0200

    refactor: Increased readability of handleMove()

--- src/App.js
+++ src/App.js
@@ -5,6 +5,13 @@ import ListBooks from './ListBooks'
 import SearchBooks from './SearchBooks'
 import './App.css'
 
+// Book Helper
+const isSameBook = (a, b) => a.id === b.id
+const containsBook = (collection, book) => (
+  collection.find((item) => item.id === book.id) ? true : false
+)
+const assignShelf = (book, shelf) => Object.assign({}, book, { shelf })
+
 class BooksApp extends React.Component {
   state = {
     myBooks: []
@@ -16,29 +23,19 @@ class BooksApp extends React.Component {
     })
   }
 
-  // TODO: Wow, this needs refactoring sooo bad
   handleMove = (book, shelf) => {
     BooksAPI.update(book, shelf).then((response) => console.log(response))
-    console.log('update', book, shelf)
-
-    const bookWithShelf = (book, shelf) => Object.assign({}, book, { shelf })
-
-    const updateShelf = (bookId, shelf) => (
-      this.state.myBooks.map((item) => (
-        (item.id === bookId)
-          ? bookWithShelf(book, shelf)
-          : item
-      ))
-    )
-
-    let index = this.state.myBooks.findIndex((item) => (item.id === book.id))
-    if (index > 0) {
-      this.setState({myBooks: updateShelf(book.id, shelf)})
-    } else {
-      this.setState({
-        myBooks: this.state.myBooks.concat([ bookWithShelf(book, shelf) ])
-      })
-    }
+    // If book is found, replace it.
+    // Otherwise add it to the myBooks array.
+    let bookOnShelf = assignShelf(book, shelf)
+    this.setState(prevState => (
+      containsBook(prevState.myBooks, book)
+        ? { myBooks: prevState.myBooks.map((someBook) =>
+              (isSameBook(someBook, book) ? bookOnShelf : someBook))
+          }
+        // Book can't be found -> Add it to the array
+        : { myBooks: prevState.myBooks.concat([ bookOnShelf ]) }
+    ))
   }
 
   render() {
