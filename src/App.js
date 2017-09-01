f4fdebbc36afc2d41b8af76a6c4291370358d48b
Carl Hüffmeier <carlhueffmeier@gmail.com>
2017-09-01 17:33:22 +0200

    refactor: Simplify handleMove() function.

--- src/App.js
+++ src/App.js
@@ -6,11 +6,9 @@ import SearchBooks from './SearchBooks'
 import './App.css'
 
 // Book Helper
-const isSameBook = (a, b) => a.id === b.id
 const containsBook = (collection, book) => (
   collection.find((item) => item.id === book.id) ? true : false
 )
-const assignShelf = (book, shelf) => Object.assign({}, book, { shelf })
 
 class BooksApp extends React.Component {
   state = {
@@ -27,14 +25,13 @@ class BooksApp extends React.Component {
     BooksAPI.update(book, shelf).then((response) => console.log(response))
     // If book is found, replace it.
     // Otherwise add it to the myBooks array.
-    let bookOnShelf = assignShelf(book, shelf)
     this.setState(prevState => (
       containsBook(prevState.myBooks, book)
         ? { myBooks: prevState.myBooks.map((someBook) =>
-              (isSameBook(someBook, book) ? bookOnShelf : someBook))
+              (someBook.id === book.id) ? { ...book, shelf } : someBook)
           }
         // Book can't be found -> Add it to the array
-        : { myBooks: prevState.myBooks.concat([ bookOnShelf ]) }
+        : { myBooks: prevState.myBooks.concat([ { ...book, shelf } ]) }
     ))
   }
 
